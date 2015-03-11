69e669cb6c070dbcccadb2dce8c39547fb6da5fd
eugenol <eug@live.co.za>
2015-03-11 23:47:32 +0200

    fix isWon and game loop

--- Tut3-TicTacToe/TicTacToecpp.cpp
+++ Tut3-TicTacToe/TicTacToecpp.cpp
@@ -58,7 +58,7 @@ bool TicTacToe::move(int row, int col)
 
 void TicTacToe::print()
 {
-	cout << " | 1 | 2 |3" << endl;
+	cout << " | 1 | 2 | 3" << endl;
 	cout << "1| " << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << endl;
 	cout << "2| " << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << endl;
 	cout << "3| " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << endl;
@@ -70,64 +70,81 @@ int TicTacToe::isWon()
 	int player1_d1 = 0, player2_d1 = 0, player1_d2 = 0, player2_d2 = 0;
 	int empty = 0;
 
-	//Check rows and columns
+	//check rows
 	for (int i = 0; i < 3; i++)
 	{
-		player1_r = player2_r = player1_c = player2_c = 0;
+		player1_r = player2_r = 0;
 
 		for (int j = 0; j < 3; j++)
 		{
-			//Check rows
 			if (board[i][j] == 'X')
-			{
 				++player1_r;
-			}
 			else if (board[i][j] == 'O')
-			{
 				++player2_r;
-			}
-			else if (board[i][j] == ' ')
-			{
+			else
 				++empty;
-			}
-			//Check columns
-			if (board[j][i] == 'X')
-			{
-				++player1_c;
-			}
-			else if (board[j][i] == 'O')
-			{
-				++player2_c;
-			}
+		}
 
+		if (player1_r == 3)
+			return PLAYER1;
+		else if (player2_r == 3)
+			return PLAYER2;
+	}
+
+	//check columns
+	for (int i = 0; i < 3; i++)
+	{
+		player1_c = player2_c = 0;
+
+		for (int j = 0; j < 3; j++)
+		{
+			if (board[j][i] == 'X')
+				++player1_c;
+			else if (board[j][i] == 'O')
+				++player2_c;
 		}
-		//Check diagonals
+
+		if (player1_c == 3)
+			return PLAYER1;
+		else if (player2_c == 3)
+			return PLAYER2;
+	}
+
+	//check diagonal 1
+	player1_d1 = player2_d1 = 0;
+	for (int i = 0; i < 3; i++)
+	{
+
 		if (board[i][i] == 'X')
-		{
 			++player1_d1;
-		}
 		else if (board[i][i] == 'O')
-		{
 			++player2_d1;
-		}
+	}
+
+	if (player1_d1 == 3)
+		return PLAYER1;
+	else if (player2_d1 == 3)
+		return PLAYER2;
+
+	//check diagonal 2
+	player1_d2 = player2_d2 = 0;
+	for (int i = 0; i < 3; i++)
+	{
 
 		if (board[2-i][i] == 'X')
-		{
-			++player1_d1;
-		}
-		else if (board[2-i][2] == 'O')
-		{
-			++player2_d1;
-		}
-
-
-		if (player1_r == 3 || player1_c == 3 || player1_d1 == 3 || player1_d2 == 3)
-			return PLAYER1;
-		else if (player2_r == 3 || player2_c == 3 || player2_d1 == 3 || player2_d2 == 3)
-			return PLAYER2;
-		else if (empty == 0)
-			return DRAW;
+			++player1_d2;
+		else if (board[2-i][i] == 'O')
+			++player2_d2;
 	}
+
+	if (player1_d2 == 3)
+		return PLAYER1;
+	else if (player2_d2 == 3)
+		return PLAYER2;
+
+	if (empty == 0)
+		return DRAW;
+
 	return IN_PROGRESS;
 }
 
--- Tut3-TicTacToe/Tut3-TicTacToe.cpp
+++ Tut3-TicTacToe/Tut3-TicTacToe.cpp
@@ -10,20 +10,27 @@ int main(/*int argc, char* argv[]*/)
 {
 	TicTacToe Game;
 	int row, col;
+	char answer;
 
-	//while (1) //Game Loop
-	//{
+	while (1) //Game Loop
+	{
+		Game.print();
 		while (Game.isWon() == TicTacToe::IN_PROGRESS)
 		{
-			Game.print();
+			//Game.print();
 			cout << "Player " << Game.getPlayer() << ". Enter your move: r,c" << endl;
 			cin >> row;
 			cin.ignore(1);
 			cin >> col;
-			Game.move(row-1, col-1);
+			if (!Game.move(row - 1, col - 1))
+				cout << "Invalid Move";
+			else
+				Game.print();
 			cout << endl;
 
 		}
+
+		Game.print();
 
 		if (Game.isWon() == TicTacToe::DRAW)
 			cout << "Game Drawn";
@@ -31,8 +38,16 @@ int main(/*int argc, char* argv[]*/)
 			cout << "Player 1 Won";
 		else if (Game.isWon() == TicTacToe::PLAYER2)
 			cout << "Player 2 Won";
-		cout << endl;
-	//}
+		cout << endl << endl;
+
+		cout << "Would you like to play again ? (Y/N)" << endl;
+		cin >> answer;
+		
+		if (answer == 'N' || answer == 'n')
+			break;
+		else if (answer == 'Y' || answer == 'y')
+			Game.restart();
+	}
 
 	return 0;
 }
